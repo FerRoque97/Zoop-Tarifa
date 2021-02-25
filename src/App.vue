73416a2ac798d9723a3b49e337f6d7d0bd663cdf
Fernando Roque <fernando.psn2014@hotmail.com>
2021-02-25 20:18:20 -0300

    Expressando todos os nomes

--- src/App.vue
+++ src/App.vue
@@ -28,113 +28,8 @@
         :items="customers"
         :items-per-page="5"
         class="elevation-1"
-<<<<<<< HEAD
-      >
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>My CRUD</v-toolbar-title>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="primary"
-                  dark
-                  class="mb-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  New Item
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
-
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.name"
-                          label="Dessert name"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.calories"
-                          label="Calories"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.fat"
-                          label="Fat (g)"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.carbs"
-                          label="Carbs (g)"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.protein"
-                          label="Protein (g)"
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="close">
-                    Cancel
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="headline"
-                  >Are you sure you want to delete this item?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete"
-                    >Cancel</v-btn
-                  >
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                    >OK</v-btn
-                  >
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
-        <template >
-
-          <!-- v-slot:item.actions="{ item }" -->
-          <v-icon small class="mr-2" @click="editItem(item)">
-            mdi-pencil
-          </v-icon>
-          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-        </template>
-        <template v-slot:no-data>
-          <v-btn color="primary" @click="initialize"> Reset </v-btn>
-        </template>
-      </v-data-table>
-    </v-flex>
-  </v-app>
-=======
       ></v-data-table> </template
   ></v-app>
->>>>>>> 12f67a220261d201a427f4d1d96e1c9c5909c308
 </template>
 
 <script>
@@ -150,9 +45,7 @@ export default {
 
   data: () => ({
     customers: [
-      {
-        name: "",
-      },
+      
     ],
     dialog: false,
     dialogDelete: false,
@@ -188,6 +81,7 @@ export default {
   }),
 
   mounted() {
+    const arrayTeste = [];
     clientes.listar().then((resposta) => {
       // console.log(resposta.data.items);
       var array = resposta.data.items;
@@ -197,22 +91,43 @@ export default {
       // this.customers = this.customers[array.length];
 
       // console.log(this.customers)
-      
-      for (let index = 0; index < array.length; index++) {
-        
-        if (array[index].type == "business") {
-        
-          this.customers[index].name = array[index].business_name;
-         
-        } else if  (array[index].type == "individual"){ 
-          
-          this.customers[index].name = array[index].first_name;
-          
+
+      array.forEach(customer => {
+        let customerName;
+        console.log(customer);
+          if(customer.type == 'business'){
+           customerName = customer.business_name;
         }
-        console.log(this.customers);
+
+          if(customer.type == 'individual'){
+           customerName = `${customer.first_name} ${customer.last_name}`
+        }
+
+        const novoCustomer = {
+          name: customerName,
+        }
+
+        this.customers.push(novoCustomer)
+
+      });
+      
+      
+
+      // for (let index = 0; index < array.length; index++) {
         
-      }
-      console.log(this.desserts)
+      //   if (array[index].type == "business") {
+        
+      //     this.customers[index].name = array[index].business_name;
+         
+      //   } else if  (array[index].type == "individual"){ 
+          
+      //     this.customers[index].name = array[index].first_name;
+          
+      //   }
+      //   console.log(this.customers);
+        
+      // }
+      // console.log(this.desserts)
       // this.desserts = this.customers;
       // console.log(this.customers);
       // console.log("Aqui troca");
